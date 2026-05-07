6aae7886e6981ae9188d9434429c7c96e34cd60c
Adamzou-lab <adamzpro@icloud.com>
2026-05-07 13:56:18 +0200

    refactor: restructure pow function to handle negative exponents correctly

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 def pow(a, b):
     result = 1
-    for i in range(b):
-        result = result * a
+    if b < 0:
+        for i in range(abs(b)):
+            result = result * a
+        result = 1 / result
+    else:
+        for i in range(b):
+            result = result * a
     return result
