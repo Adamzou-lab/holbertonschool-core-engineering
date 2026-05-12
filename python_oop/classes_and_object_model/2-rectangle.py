837fbaa375c56774bb1b94ccac1c3c9f523c3d94
Adamzou-lab <adamzpro@icloud.com>
2026-05-12 18:47:57 +0200

    feat: implement Rectangle class with width and height properties

---
+++ python_oop/classes_and_object_model/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Module that defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self, width, height):
+        if width == 0 or height == 0:
+            self.perimeter = 0
+        return 2 * (width + height)
