304a5451c5be8f6d40883088298045a4bd3883cf
Adamzou-lab <adamzpro@icloud.com>
2026-05-12 18:50:30 +0200

    fix: correct perimeter method to use instance attributes instead of parameters

--- python_oop/classes_and_object_model/2-rectangle.py
+++ python_oop/classes_and_object_model/2-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
     def area(self):
         return self.__width * self.__height
 
-    def perimeter(self, width, height):
-        if width == 0 or height == 0:
-            self.perimeter = 0
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
