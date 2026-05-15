b73463b856bac0c21ee390f090e10a78b61ca25e
Adamzou-lab <adamzpro@icloud.com>
2026-05-15 20:02:29 +0200

    fix: remove unnecessary article in habitat method of FlyingFish class

--- python_oop/abc/flyingfish.py
+++ python_oop/abc/flyingfish.py
@@ -31,7 +31,7 @@ class FlyingFish(Fish, Bird):
         print("The flying fish is swimming!")
 
     def habitat(self):
-        print("The flying fish lives both in the water and the sky!")
+        print("The flying fish lives both in water and the sky!")
 
 
 fish = FlyingFish()
