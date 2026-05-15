187955bcc734c7fcb73e2ef6592863b37098c287
Adamzou-lab <adamzpro@icloud.com>
2026-05-15 20:00:40 +0200

    feat: add FlyingFish class with multiple inheritance from Fish and Bird

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Module that defines Fish, Bird and FlyingFish classes
+with multiple inheritance"""
+
+
+class Fish:
+    """Concrete class representing a fish"""
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """Concrete class representing a bird"""
+    def fly(self):
+        print("The bird is fying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Concrete class representing a flying fish that inherits
+    from both Fish and Bird"""
+    def swim(self):
+        print("The flying fish is soaring!")
+
+    def fly(self):
+        print("The flying fish is swimming")
+
+    def habitat(self):
+        print("The flying fish lives both in the water and the sky!")
+
+
+fish = FlyingFish()
+
+fish.fly()
+fish.swim()
+fish.habitat()
+
+print(FlyingFish.__mro__)
